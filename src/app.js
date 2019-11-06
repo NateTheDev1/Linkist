40d7bbbd90165aa91d8644469b8d9811880d57ed
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2019-11-06 11:44:11 -0500

    [NEW] Added http AUTH

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ let linkList = document.getElementById('link-list');
 submit.addEventListener('click', function() {
 	let name = document.getElementById('link-title');
 	let url = document.getElementById('link-url');
-	if (name.length !== 0 && url.length !== 0) {
+	if (name.value.length !== 0 && url.value.length !== 0) {
 		injectNew(name.value, url.value);
 		name.value = '';
 		url.value = '';
@@ -14,5 +14,16 @@ submit.addEventListener('click', function() {
 });
 
 function injectNew(name, url) {
-	linkList.innerHTML += `<li class="list-group-item">${name} <a class="btn btn-primary" href="${url}" target="_blank">Visit</a></li>`;
+	let newUrl = checkURL(url);
+	linkList.innerHTML += `<li class="list-group-item">${name} <a class="btn btn-primary" href="${newUrl}" target="_blank">Visit</a></li>`;
+}
+
+function checkURL (url) {
+	if(url.includes('http://'))
+	{
+		return;
+	} else {
+		let newUrl = 'http://' + url;
+		return newUrl;
+	}
 }
