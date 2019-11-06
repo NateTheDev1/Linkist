64b9715832a0be0e409ca062316a23656bff78d9
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2019-11-06 16:00:45 -0500

    [HFIX]

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 let submit = document.getElementById('submit-new');
 let linkList = document.getElementById('link-list');
+let name = document.getElementById('link-title');
+let url = document.getElementById('link-url');
+
+name.addEventListener('keyup', function(event) {
+	if (event.keycode === 13) {
+		event.preventDefault();
+		submit.click();
+	}
+});
 
 submit.addEventListener('click', function() {
-	let name = document.getElementById('link-title');
-	let url = document.getElementById('link-url');
 	if (name.value.length !== 0 && url.value.length !== 0) {
 		injectNew(name.value, url.value);
 		name.value = '';
@@ -18,9 +25,8 @@ function injectNew(name, url) {
 	linkList.innerHTML += `<li class="list-group-item">${name} <a class="btn btn-primary" href="${newUrl}" target="_blank">Visit</a></li>`;
 }
 
-function checkURL (url) {
-	if(url.includes('http://'))
-	{
+function checkURL(url) {
+	if (url.includes('http://')) {
 		return;
 	} else {
 		let newUrl = 'http://' + url;
