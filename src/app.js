cd9223968f3ea4f1c9c5388376740f81d206cfeb
NateTheDev1 <46858886+NateTheDev1@users.noreply.github.com>
2019-11-08 02:35:45 -0500

    Implemented Git Quote

--- src/app.js
+++ src/app.js
@@ -3,6 +3,23 @@ let submit = document.getElementById('submit-new');
 let linkList = document.getElementById('link-list');
 let name = document.getElementById('link-title');
 let url = document.getElementById('link-url');
+let gitQuote = document.getElementById('git-quote');
+
+// GIT QUOTE HANDLER
+let xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+	if (xhr.readyState == 4) {
+		if (xhr.status == 200) {
+			gitQuote.innerText = xhr.responseText;
+		} else {
+			gitQuote.innerText = 'Code On!';
+		}
+	}
+};
+
+xhr.open('GET', 'https://api.github.com/zen');
+xhr.send();
 
 // EVENTS TO LISTEN TO ENTER KEY
 name.addEventListener('keyup', function(event) {
